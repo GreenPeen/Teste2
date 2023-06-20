0e11e18db49433f6f8df4c18277148c23ac5c3dc
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-06-20 19:50:39 -0300

    Apagaram uma coisa, porra

--- demo/src/main/java/com/example/demo/controller/view/MainControllerView.java
+++ demo/src/main/java/com/example/demo/controller/view/MainControllerView.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping(path = "/home")
 public class MainControllerView {
 	
 	@GetMapping
